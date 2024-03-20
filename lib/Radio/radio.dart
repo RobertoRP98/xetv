fe36f3740d16d3e3b652c1b2745fdaf72600da8a
RobertoRP98 <robertorpsistemas@gmail.com>
2024-03-20 16:48:04 -0600

    Radio V1 con Just_audio

--- lib/Radio/radio.dart
+++ lib/Radio/radio.dart
@@ -1,5 +1,5 @@
-import "package:audioplayers/audioplayers.dart";
 import "package:flutter/material.dart";
+import 'package:just_audio/just_audio.dart';
 import "package:xetv/Radio/gradient.dart";
 class radio extends StatefulWidget {
   const radio({super.key});
@@ -7,70 +7,72 @@ class radio extends StatefulWidget {
   State<radio> createState() => _radioState();
 }
 class _radioState extends State<radio> {
-    AudioPlayer audioPlayer = AudioPlayer();
-    bool playing = false;
-     @override
-  void initState() {
+  late AudioPlayer _audioPlayer;
+
+  @override
+  void initState(){
     super.initState();
-    audioPlayer = AudioPlayer();
+    _audioPlayer = AudioPlayer()..setUrl('https://radio.golsystems.com.mx/8202/stream');
   }
 
-  void playAudio()async{
-    var url = 'https://radio.golsystems.com.mx/8202/stream';
-    if(playing){
-      await audioPlayer.pause();
-        setState(() {
-          playing=false;
-        });
-    }else{
-      await audioPlayer.play(UrlSource(url),
-    ); 
-        setState(() {
-          playing = true;
-        });
-    }
+  @override
+  void dispose(){
+    _audioPlayer.dispose();
+    super.dispose();
   }
 
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
+      appBar: AppBar(
         title: const Text('Radio en vivo'),
       ),
-      body: gradient( 
-        child: Container(
-          padding: const EdgeInsets.all(30),
-          child: Column( 
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-           const Image(image: AssetImage('images/logo.png')),
-              InkWell( 
-                 onTap: () {
-                  if (playing) {
-                    audioPlayer.pause();
-                    setState(() {
-                      playing = false;
-                    });
-                  } else {
-                    playAudio();
-                    setState(() {
-                      playing = true;
-                    });
-                  }
-                },
-                child: Icon( 
-                  playing == false
-                  ? Icons.play_circle_fill_outlined
-                  : Icons.pause_circle_outline,
-                  size: 100,
-                  color: Colors.yellowAccent,
-
-                ),
-              ),
-            ],
-          ),
-        ),
-      ) ,
+      body: gradient(
+        child: Controls(audioPlayer: _audioPlayer),
+      ),
     );
   }
 }
 
+class Controls extends StatelessWidget{
+  const Controls({
+    super.key,
+    required this.audioPlayer,
+  });
+  final AudioPlayer audioPlayer;
+
+  @override 
+  @override 
+Widget build(BuildContext context) {
+  return StreamBuilder<PlayerState>(
+    stream: audioPlayer.playerStateStream,
+    builder: (context, snapshot) {
+      final playerState = snapshot.data;
+      final processingState = playerState?.processingState;
+      final playing = playerState?.playing;
+
+      if (!(playing ?? false)) {
+        return IconButton(
+          onPressed: audioPlayer.play,
+          iconSize: 80,
+          color: Colors.pink,
+          icon: const Icon(Icons.play_arrow_rounded),
+        );
+      } else if (processingState != ProcessingState.completed) {
+        return IconButton(
+          onPressed: audioPlayer.pause,
+          iconSize: 80,
+          color: Colors.amber,
+          icon: const Icon(Icons.pause_rounded),
+        );
+      }
+      return const Icon(
+        Icons.play_arrow_rounded,
+        size: 80,
+        color: Colors.blueGrey,
+      );
+    },
+  );
+}
+
+}

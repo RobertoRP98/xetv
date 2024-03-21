63e2254caaf7a67b2e5c5ea13860f24e8519b82d
RobertoRP98 <robertorpsistemas@gmail.com>
2024-03-21 17:56:25 -0600

    Just_audio V1.1

--- lib/Radio/radio.dart
+++ lib/Radio/radio.dart
@@ -1,6 +1,10 @@
+import "package:cached_network_image/cached_network_image.dart";
 import "package:flutter/material.dart";
 import 'package:just_audio/just_audio.dart';
+import "package:just_audio_background/just_audio_background.dart";
 import "package:xetv/Radio/gradient.dart";
+
+
 class radio extends StatefulWidget {
   const radio({super.key});
   @override
@@ -9,10 +13,27 @@ class radio extends StatefulWidget {
 class _radioState extends State<radio> {
   late AudioPlayer _audioPlayer;
 
+  final _playlist = ConcatenatingAudioSource(
+    children: [
+      AudioSource.uri(Uri.parse('https://radio.golsystems.com.mx/8202/stream'),
+        tag: MediaItem(id: '0',
+         title: 'Radio en vivo XHVX',
+         artist: '89.7 FM',
+         artUri: Uri.parse('images/logo.png'),
+         )
+      )
+    ],
+  );
+
   @override
   void initState(){
     super.initState();
-    _audioPlayer = AudioPlayer()..setUrl('https://radio.golsystems.com.mx/8202/stream');
+    _audioPlayer = AudioPlayer();
+    _init();
+  }
+
+  Future<void> _init() async{
+    await _audioPlayer.setAudioSource(_playlist);
   }
 
   @override
@@ -22,16 +43,26 @@ class _radioState extends State<radio> {
   }
 
 @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Radio en vivo'),
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Radio en vivo'),
+    ),
+    body: gradient(
+      child: Column(
+        children: [
+          Controls(audioPlayer: _audioPlayer),
+          MediaMetadata(
+            imageUrl: 'URL_DE_LA_IMAGEN',
+            title: 'Título de la canción',
+            artist: 'Nombre del artista',
+          ),
+        ],
       ),
-      body: gradient(
-        child: Controls(audioPlayer: _audioPlayer),
-      ),
-    );
-  }
+    ),
+  );
+}
+
 }
 
 class Controls extends StatelessWidget{
@@ -41,7 +72,6 @@ class Controls extends StatelessWidget{
   });
   final AudioPlayer audioPlayer;
 
-  @override 
   @override 
 Widget build(BuildContext context) {
   return StreamBuilder<PlayerState>(
@@ -74,5 +104,66 @@ Widget build(BuildContext context) {
     },
   );
 }
-
 }
+
+class MediaMetadata extends StatelessWidget{
+  const MediaMetadata({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.artist,
+  });
+  final String imageUrl;
+  final String title;
+  final String artist;
+
+  @override
+  Widget build(BuildContext context){
+    return Column(children: [
+      DecoratedBox(decoration: BoxDecoration( 
+        boxShadow: const [
+          BoxShadow(
+            color:Colors.black12,
+            offset: Offset(2 , 4 ),
+            blurRadius: 4,
+          ),
+        ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ClipRRect( 
+        borderRadius: BorderRadius.circular(10),
+        child: CachedNetworkImage( 
+          imageUrl: imageUrl,
+          height:300,
+          width:300,
+          fit:BoxFit.cover,
+        ),
+      ),
+      ),
+     
+//4:30
+
+      const SizedBox(height: 20),
+      Text( 
+        artist,
+        style: const TextStyle( 
+          color: Colors.amberAccent,
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      const SizedBox(height: 8),
+      Text(
+        artist,
+        style: const TextStyle( 
+          color: Colors.red,
+          fontSize: 20,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ]);
+  }
+}
+
+

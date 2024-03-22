4765bc2ca8761bb84200badbb1940bf29f4922e2
RobertoRP98 <robertorpsistemas@gmail.com>
2024-03-22 16:47:19 -0600

    Just_audio V1.2 sin Notificacion

--- lib/Radio/radio.dart
+++ lib/Radio/radio.dart
@@ -1,169 +1,185 @@
-import "package:cached_network_image/cached_network_image.dart";
 import "package:flutter/material.dart";
 import 'package:just_audio/just_audio.dart';
+import "package:cached_network_image/cached_network_image.dart";
 import "package:just_audio_background/just_audio_background.dart";
 import "package:xetv/Radio/gradient.dart";
 
-
 class radio extends StatefulWidget {
-  const radio({super.key});
+  const radio({Key? key}) : super(key: key);
+
   @override
   State<radio> createState() => _radioState();
 }
+
 class _radioState extends State<radio> {
   late AudioPlayer _audioPlayer;
 
   final _playlist = ConcatenatingAudioSource(
     children: [
-      AudioSource.uri(Uri.parse('https://radio.golsystems.com.mx/8202/stream'),
-        tag: MediaItem(id: '0',
-         title: 'Radio en vivo XHVX',
-         artist: '89.7 FM',
-         artUri: Uri.parse('images/logo.png'),
-         )
+      AudioSource.uri(
+        Uri.parse('https://radio.golsystems.com.mx/8202/stream'),
+        tag: MediaItem(
+          id: '0',
+          title: 'Radio en vivo XHVX',
+          artist: '89.7 FM',
+          artUri: Uri.parse('https://www.grupovx.com/src/logo-vx.png'),
+          
+        ),
       )
     ],
   );
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _audioPlayer = AudioPlayer();
     _init();
   }
 
-  Future<void> _init() async{
+  Future<void> _init() async {
     await _audioPlayer.setAudioSource(_playlist);
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _audioPlayer.dispose();
     super.dispose();
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('Radio en vivo'),
-    ),
-    body: gradient(
-      child: Column(
-        children: [
-          Controls(audioPlayer: _audioPlayer),
-          MediaMetadata(
-            imageUrl: 'URL_DE_LA_IMAGEN',
-            title: 'Título de la canción',
-            artist: 'Nombre del artista',
-          ),
-        ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Radio en vivo'),
       ),
-    ),
-  );
+      body: gradient(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder<SequenceState?>(
+              stream: _audioPlayer.sequenceStateStream,
+              builder: (context, snapshot) {
+                final state = snapshot.data;
+                if (state?.sequence.isEmpty ?? true) {
+                  return const SizedBox();
+                }
+                final metadata =
+                    state!.currentSource!.tag as MediaItem;
+                return MediaMetadata(
+                  imageUrl: metadata.artUri.toString(),
+                  artist: '',
+                  title: '',
+                );
+              },
+            ),
+            Controls(audioPlayer: _audioPlayer),
+
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-}
-
-class Controls extends StatelessWidget{
+class Controls extends StatelessWidget {
   const Controls({
-    super.key,
+    Key? key,
     required this.audioPlayer,
-  });
+  }) : super(key: key);
   final AudioPlayer audioPlayer;
 
-  @override 
-Widget build(BuildContext context) {
-  return StreamBuilder<PlayerState>(
-    stream: audioPlayer.playerStateStream,
-    builder: (context, snapshot) {
-      final playerState = snapshot.data;
-      final processingState = playerState?.processingState;
-      final playing = playerState?.playing;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<PlayerState>(
+      stream: audioPlayer.playerStateStream,
+      builder: (context, snapshot) {
+        final playerState = snapshot.data;
+        final processingState = playerState?.processingState;
+        final playing = playerState?.playing;
 
-      if (!(playing ?? false)) {
-        return IconButton(
-          onPressed: audioPlayer.play,
-          iconSize: 80,
-          color: Colors.pink,
-          icon: const Icon(Icons.play_arrow_rounded),
+        if (!(playing ?? false)) {
+          return IconButton(
+            onPressed: audioPlayer.play,
+            iconSize: 120,
+            color: Colors.black54,
+            icon: const Icon(Icons.play_arrow_rounded),
+
+          );
+        } else if (processingState != ProcessingState.completed) {
+          return IconButton(
+            onPressed: audioPlayer.pause,
+            iconSize: 120,
+            color: Colors.black54,
+            icon: const Icon(Icons.pause_rounded),
+          );
+        }
+        return const Icon(
+          Icons.play_arrow_rounded,
+          size: 80,
+          color: Colors.blueGrey,
         );
-      } else if (processingState != ProcessingState.completed) {
-        return IconButton(
-          onPressed: audioPlayer.pause,
-          iconSize: 80,
-          color: Colors.amber,
-          icon: const Icon(Icons.pause_rounded),
-        );
-      }
-      return const Icon(
-        Icons.play_arrow_rounded,
-        size: 80,
-        color: Colors.blueGrey,
-      );
-    },
-  );
-}
+      },
+    );
+  }
 }
 
-class MediaMetadata extends StatelessWidget{
+class MediaMetadata extends StatelessWidget {
   const MediaMetadata({
-    super.key,
+    Key? key,
     required this.imageUrl,
     required this.title,
     required this.artist,
-  });
+  }) : super(key: key);
   final String imageUrl;
   final String title;
   final String artist;
 
   @override
-  Widget build(BuildContext context){
-    return Column(children: [
-      DecoratedBox(decoration: BoxDecoration( 
-        boxShadow: const [
-          BoxShadow(
-            color:Colors.black12,
-            offset: Offset(2 , 4 ),
-            blurRadius: 4,
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        DecoratedBox(
+          decoration: BoxDecoration(
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.transparent,
+                offset: Offset(2, 4),
+                blurRadius: 4,
+              ),
+            ],
+            borderRadius: BorderRadius.circular(10),
           ),
-        ],
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: ClipRRect( 
-        borderRadius: BorderRadius.circular(10),
-        child: CachedNetworkImage( 
-          imageUrl: imageUrl,
-          height:300,
-          width:300,
-          fit:BoxFit.cover,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: CachedNetworkImage(
+              imageUrl: imageUrl,
+              height: 300,
+              width: 300,
+              fit: BoxFit.none
+            ),
+          ),
         ),
-      ),
-      ),
-     
-//4:30
-
-      const SizedBox(height: 20),
-      Text( 
-        artist,
-        style: const TextStyle( 
-          color: Colors.amberAccent,
-          fontSize: 22,
-          fontWeight: FontWeight.bold,
+        const SizedBox(height: 20),
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.yellowAccent,
+            fontSize: 0,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
         ),
-        textAlign: TextAlign.center,
-      ),
-      const SizedBox(height: 8),
-      Text(
-        artist,
-        style: const TextStyle( 
-          color: Colors.red,
-          fontSize: 20,
+        const SizedBox(height: 8),
+        Text(
+          artist,
+          style: const TextStyle(
+            color: Colors.redAccent,
+            fontSize: 0,
+          ),
+          textAlign: TextAlign.center,
         ),
-        textAlign: TextAlign.center,
-      ),
-    ]);
+      ],
+    );
   }
 }
-
 
